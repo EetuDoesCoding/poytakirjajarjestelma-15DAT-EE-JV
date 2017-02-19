8871cb9bcd26562f47ab9aa8892edf35e78733df
EetuDoesCoding <eetuelo@gmail.com>
2017-02-19 12:07:49 +0200

    19.2

--- etusivu.php
+++ etusivu.php
@@ -3,16 +3,54 @@
 <head>
   <meta charset="utf-8">
   <title><?php $_SERVER['PHP_SELF'] ?></title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundaiton.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation-icons/3.0/foundaiton-icons.min.css">
-  <style>
-  <?php
-
-  ?>
-  </style>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  
 </head>
 <body>
+<header>
+<div class="row">
+<div class="small-12 columns">
+<h1>Kokkolan Kaupungin Tietokanta</h1>
+</div>
+</div>
+</header>
+<nav>
+<div class="row">
+<div class="small-12 columns">
+<ul>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn"
+    onclick="myFunction()">Kirjaudu</a>
+    <div class="dropdown-content" id="myDropdown">
+    <form autocomplete="off" method="POST" action="hallinta.php">
+    Käyttäjätunnus:<br>
+    <input type="input" name="login" value="" placeholder="Käyttäjätunnus">
+    Salasana:<br>
+    <input title="Salasana" type="password" name="passwd"
+    value="">
+    <button name="btn" value="1">Kirjaudu</button>
+    </form>
 
-<script src="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.js"></script>
+    </div>
+  </li>
+</ul>
+</div>
+</div>
+
+</nav>
+<article>
+
+
+</article>
+
+<script>
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+</script>
 </body>
 </html>
